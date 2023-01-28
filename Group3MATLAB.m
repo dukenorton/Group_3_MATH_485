8345dbffa1514eb6f3440addbb5a7bdba7038e79
dukenorton <123835536+dukenorton@users.noreply.github.com>
2023-01-28 13:57:20 -0700

    Add files via upload

---
+++ Group3MATLAB.m
@@ -0,0 +1,10 @@
+%{ 
+Math 485 
+Andrew, Duke, John, Nikhil 
+%}
+
+%{
+ Write a code to simulate the coupled ODEs, explore different
+vaccine rollout scenarios, and reproduce the results of the article.
+%}
+
